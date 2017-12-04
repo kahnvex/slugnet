c63c8fd269cca463822513965a2aa28897a8744e
Jarrod Kahn <jarrod.kahn@vevo.com>
2017-12-04 13:49:47 -0800

    Add doc deps

--- slugnet/layers.py
+++ slugnet/layers.py
@@ -8,6 +8,40 @@ class Layer(object):
 
 
 class Dense(Layer):
+    """
+    A common densely connected neural network layer.
+
+    The :code:`Dense` layer implements the feed forward operation
+
+    .. math::
+        :nowrap:
+
+        \[
+            \mathbf{z} = g(\mathbf{W}_i^T \mathbf{x} + \mathbf{b})
+        \]
+
+    where :math:`z` is output, :math:`g` is the activation
+    function, :math:`W` are weights, :math:`b` is our
+    bias, and :math:`i` is the index of the current layer.
+
+    On feed backward, or backpropogation, the :code:`Dense` layer
+    calculates two values as follows
+
+    .. math::
+        :nowrap:
+
+        \\begin{flalign}
+            \\frac{\partial \ell}{\partial \\bm{z}^{(i)}} &=
+                g'(z^{(i)}) .*\
+                \Big[ \\bm{W}^{(i + 1)^T} \\frac{\partial \ell}{\partial z^{(i + 1)}}\Big] \\\\
+            \\frac{\partial \ell}{\partial \\bm{W}^{(i)}} &= \\frac{\partial \ell}{\partial \\bm{z}^{(i)}} \\bm{x}^T
+        \\end{flalign}
+
+    When looking at the source, there is a notable absence of
+    :math:`\\bm{W}^T` and :math:`\\frac{\partial \ell}{\partial z^{(i + 1)}}`.
+    This is because their dot product is calculated in the previous layer.
+    The model propogates that gradient to this layer.
+    """
     def __init__(self, ind, outd, activation=Noop()):
         self.shape = ind, outd
         self.w = np.random.normal(0, 0.1, self.shape)
