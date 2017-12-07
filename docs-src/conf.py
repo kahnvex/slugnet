759d8c52822343f9c504d6e0e9d340d65f87a295
Jarrod Kahn <jarrod.kahn@vevo.com>
2017-12-06 18:56:19 -0800

    Expand on backpropogation section

--- docs-src/conf.py
+++ docs-src/conf.py
@@ -144,7 +144,7 @@ latex_elements = {
 
     # Additional stuff for the LaTeX preamble.
     #
-    'preamble': '\usepackage{tkiz}\n\usetikzlibrary{positioning}\n\usepackage{bm}',
+    'preamble': '\usepackage{tkiz}\n\usetikzlibrary{positioning}\n\usepackage{bm}\n\usepackage{amsmath}',
 
     # Latex figure (float) alignment
     #
