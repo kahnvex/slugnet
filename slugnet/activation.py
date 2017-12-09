c58bb823653a51de459c514a29cbfe05a276d546
Jarrod Kahn <jarrod.kahn@gmail.com>
2017-12-08 20:10:12 -0800

    Add necessary components to train an mnist classifier

--- slugnet/activation.py
+++ slugnet/activation.py
@@ -23,3 +23,38 @@ class Noop(Activation):
 
     def derivative(self, x=1.):
         return x
+
+
+class ReLU(Activation):
+
+    def __init__(self):
+        super(ReLU, self).__init__()
+
+    def call(self, x):
+        self.last_forward = x
+
+        return np.maximum(0.0, x)
+
+    def derivative(self, x=None):
+        last_forward = x if x else self.last_forward
+        res = np.zeros(last_forward.shape, dtype='float32')
+        res[last_forward > 0] = 1.
+
+        return res
+
+
+class Softmax(Activation):
+    def __init__(self):
+        super(Softmax, self).__init__()
+
+    def call(self, x):
+        assert np.ndim(x) == 2
+        self.last_forward = x
+        x = x - np.max(x, axis=1, keepdims=True)
+        exp_x = np.exp(x)
+        s = exp_x / np.sum(exp_x, axis=1, keepdims=True)
+        return s
+
+    def derivative(self, x=None):
+        last_forward = x if x else self.last_forward
+        return np.ones(last_forward.shape, dtype='float32')
--- slugnet/loss.py
+++ slugnet/loss.py
@@ -31,53 +31,29 @@ class Objective(object):
 
 
 class BinaryCrossEntropy(Objective):
-    """Computes the binary cross-entropy between predictions and targets.
-
-    Returns
-    -------
-    numpy array
-        An expression for the element-wise binary cross-entropy.
-    Notes
-    -----
-    This is the loss function of choice for binary classification problems
-    and sigmoid output units.
-
-    """
     def __init__(self, epsilon=1e-11):
         self.epsilon = epsilon
 
     def forward(self, yh, y):
-        """Forward pass.
-
-        .. math::
-            :nowrap:
-
-            \\begin{flalign}
-                &L_i = y_i \\log(\hat{y}_i) +
-                    (1 - y_i) \\log(1 - \hat{y}_i) \\\\
-                &L = -\\frac{1}{N} \sum_{i=1}^N L_i
-            \\end{flalign}
-
-        Parameters
-        ----------
-        outputs : numpy.array
-            Predictions in (0, 1), such as sigmoidal output of a neural network.
-        targets : numpy.array
-            Targets in [0, 1], such as ground truth labels.
-        """
         yh = np.clip(y, self.epsilon, 1 - self.epsilon)
         loss = -np.sum(y * np.log(yh) + (1 - y) * np.log(1 - yh), axis=1)
         return np.mean(loss)
 
     def backward(self, outputs, targets):
-        """Backward pass.
-        Parameters
-
-        outputs : numpy.array
-            Predictions in (0, 1), such as sigmoidal output of a neural network.
-        targets : numpy.array
-            Targets in [0, 1], such as ground truth labels.
-        """
         outputs = np.clip(outputs, self.epsilon, 1 - self.epsilon)
         divisor = np.maximum(outputs * (1 - outputs), self.epsilon)
+
         return (outputs - targets) / divisor
+
+
+class SoftmaxCategoricalCrossEntropy(Objective):
+    def __init__(self, epsilon=1e-11):
+        self.epsilon = epsilon
+
+    def forward(self, outputs, targets):
+        outputs = np.clip(outputs, self.epsilon, 1 - self.epsilon)
+        return np.mean(-np.sum(targets * np.log(outputs), axis=1))
+
+    def backward(self, outputs, targets):
+        outputs = np.clip(outputs, self.epsilon, 1 - self.epsilon)
+        return outputs - targets
