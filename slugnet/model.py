64a74f24381e09db4111c33c160e605d8be8627e
Jarrod Kahn <jarrod.kahn@vevo.com>
2017-12-08 16:50:24 -0800

    Update model docstrings

--- slugnet/model.py
+++ slugnet/model.py
@@ -5,6 +5,10 @@ from slugnet.loss import BinaryCrossEntropy
 
 
 class Model(object):
+    """
+    Models implement functionality for fitting neural networks and
+    making predictions.
+    """
     def __init__(self, lr=0.1, n_epoch=400000, n_batches=1, layers=[], l1=0.0,
                  l2=0.0, optimizer=SGD(), loss=BinaryCrossEntropy()):
         self.layers = layers
@@ -30,6 +34,9 @@ class Model(object):
             grad = layer.backprop(grad)
 
     def fit(self, X, y):
+        """
+        Train the model given samples :code:`X` and labels or values :code`y`.
+        """
         for epoch in range(self.n_epoch):
             X_mb = np.array_split(X, self.n_batches)
             y_mb = np.array_split(y, self.n_batches)
@@ -50,4 +57,7 @@ class Model(object):
                 self.optimizer.update(params, grads)
 
     def transform(self, X):
+        """
+        Predict the labels or values of some input matrix :code:`X`.
+        """
         return self._forward(X)[0]
