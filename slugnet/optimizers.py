6c2d3a2b81c1f766b3171bc4a4f9919cf6168491
Jarrod Kahn <jarrod.kahn@gmail.com>
2017-12-09 17:40:49 -0800

    Add and use RMSProp for mnist model test

--- slugnet/optimizers.py
+++ slugnet/optimizers.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+from slugnet.initializations import _zero
+
 
 class Optimizer(object):
     def __init__(self, lr=0.001, clip=-1, decay=0., lr_min=0., lr_max=np.inf):
@@ -46,3 +48,51 @@ def npdl_clip(grad, boundary):
         return np.clip(grad, -boundary, boundary)
     else:
         return grad
+
+
+class RMSProp(Optimizer):
+    """RMSProp updates
+    Scale learning rates by dividing with the moving average of the root mean
+    squared (RMS) gradients. See [1]_ for further description.
+    Parameters
+    ----------
+    rho : float
+        Gradient moving average decay factor.
+    epsilon : float
+        Small value added for numerical stability.
+    Notes
+    -----
+    `rho` should be between 0 and 1. A value of `rho` close to 1 will decay the
+    moving average slowly and a value close to 0 will decay the moving average
+    fast.
+    Using the step size :math:`\\eta` and a decay factor :math:`\\rho` the
+    learning rate :math:`\\eta_t` is calculated as:
+    .. math::
+       r_t &= \\rho r_{t-1} + (1-\\rho)*g^2\\\\
+       \\eta_t &= \\frac{\\eta}{\\sqrt{r_t + \\epsilon}}
+    References
+    ----------
+    .. [1] Tieleman, T. and Hinton, G. (2012):
+           Neural Networks for Machine Learning, Lecture 6.5 - rmsprop.
+           Coursera. http://www.youtube.com/watch?v=O3sxAc4hxZU (formula @5:20)
+    """
+
+    def __init__(self, rho=0.9, epsilon=1e-6, *args, **kwargs):
+        super(RMSProp, self).__init__(*args, **kwargs)
+
+        self.rho = rho
+        self.epsilon = epsilon
+
+        self.cache = None
+        self.iterations = 0
+
+    def update(self, params, grads):
+        # init cache
+        if self.cache is None:
+            self.cache = [_zero(p.shape) for p in params]
+
+        # update parameters
+        for i, (c, p, g) in enumerate(zip(self.cache, params, grads)):
+            c = self.rho * c + (1 - self.rho) * np.power(g, 2)
+            p -= (self.lr * g / np.sqrt(c + self.epsilon))
+            self.cache[i] = c
