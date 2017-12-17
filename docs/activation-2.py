58c140be746e8e684b7693270f9f3c2f0eb2275d
Jarrod Kahn <jarrod.kahn@vevo.com>
2017-12-17 11:14:24 -0800

    Improve activation doc ordering

--- docs/activation-2.py
+++ docs/activation-2.py
@@ -1,17 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-z = np.arange(-4, 4, .01)
-gz = 1 / (1 + np.exp(-z))
+z = np.arange(-2, 2, .01)
+y = np.tanh(z)
 
 fig = plt.figure()
 ax = fig.add_subplot(111)
-ax.plot(z, gz)
-ax.set_ylim([0.0, 1.0])
-ax.set_xlim([-4.0, 4.0])
+ax.plot(z, y)
+ax.set_ylim([-1.0, 1.0])
+ax.set_xlim([-2.0, 2.0])
 ax.grid(True)
 ax.set_xlabel('z')
 ax.set_ylabel('g(z)')
-ax.set_title('Sigmoid')
+ax.set_title('Hyperbolic Tangent')
 
 plt.show()
