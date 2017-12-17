d1d66f48496f8309d646a34b13e3f1ea0f2f88dc
Jarrod Kahn <jarrod.kahn@vevo.com>
2017-12-17 11:59:04 -0800

    Make activation notation more consistent

--- docs/activation-1.py
+++ docs/activation-1.py
@@ -12,7 +12,7 @@ ax.set_ylim([-1.0, 2.0])
 ax.set_xlim([-2.0, 2.0])
 ax.grid(True)
 ax.set_xlabel('z')
-ax.set_ylabel('g(z)')
+ax.set_ylabel('phi(z)')
 ax.set_title('Rectified linear unit')
 
 plt.show()
